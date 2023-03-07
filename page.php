6c1155401a11fbd23158ff1298abac26bc4d8b38
Simjsh <sim.joseph@hotmail.com>
2023-03-07 22:46:30 +0100

    1

--- page.php
+++ page.php
@@ -20,7 +20,11 @@
         $moisTxt = $lMois[intval($mLundi)-1].' - '.$lMois[intval($mDimanche)-1];
     }
 
+    session_start();
 
+    $files = ["http://ade.univ-tours.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=f998fbd46b339e409e6c6cbde361edb9c3489fd34f432173475bc090f1723f925bded9a340d88a4358f846a7a4472b4dfe28586de380bafe174e6e49b572b07d,1"/*, "http://ade.univ-tours.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=799faf6bf0b6a9fce5a794983d0c862d15dcab0f24b8913b5d144e852a5d9fe58a209302a3a57afb3553857383c37db83b35a62046be8482c8e9a74d112f972a,1"*/];
+
+    $_SESSION["liens"]=$files;
 
 ?>
 
@@ -34,14 +38,23 @@
 </head>
 <body>
 
+<header>
+
 <div id="autreSem">
-    <a href="page.php?w=<?php echo $nSemaine-1 ?>"><</a>
-    <a href="page.php">Cette semaine</a>
-    <a href="page.php?w=<?php echo $nSemaine+1 ?>">></a>
+    <a id="boutonAjd" href="page.php">Aujourd'hui</a>
+    <a class="bF" href="page.php?w=<?php echo $nSemaine-1 ?>"><</a>
+    <a class="bF" href="page.php?w=<?php echo $nSemaine+1 ?>">></a>
     <?php echo $moisTxt ;?>
 </div>
 
-<div >
+</header>
+
+<div id="contenu">
+
+    <div id="gauche">
+        <input type="date" name="date">
+    </div>
+
 	<?php include 'tableau.php' ?>
 </div>
 
--- tableau.php
+++ tableau.php
@@ -59,9 +59,13 @@ function getIcsValue($eventString, $fieldName) {
     return trim($matches[2]);
 }
 
-$file = "http://ade.univ-tours.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=f998fbd46b339e409e6c6cbde361edb9c3489fd34f432173475bc090f1723f925bded9a340d88a4358f846a7a4472b4dfe28586de380bafe174e6e49b572b07d,1";
+$files = ["http://ade.univ-tours.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=f998fbd46b339e409e6c6cbde361edb9c3489fd34f432173475bc090f1723f925bded9a340d88a4358f846a7a4472b4dfe28586de380bafe174e6e49b572b07d,1"/*, "http://ade.univ-tours.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=799faf6bf0b6a9fce5a794983d0c862d15dcab0f24b8913b5d144e852a5d9fe58a209302a3a57afb3553857383c37db83b35a62046be8482c8e9a74d112f972a,1"*/];
 
-$events = getIcsEvents($file);
+$events = array();
+
+foreach($_SESSION["liens"] as $file) {
+    $events = array_merge($events, getIcsEvents($file));
+}
 
 function comparer_dates($a, $b) {
     $timestamp_a = $a['date']->getTimestamp();
@@ -72,14 +76,14 @@ function comparer_dates($a, $b) {
     return ($timestamp_a < $timestamp_b) ? -1 : 1;
 }
 
-usort($events, 'comparer_dates');
+$events = array_map('unserialize', array_unique(array_map('serialize', $events))); // On enlève les doublons
+usort($events, 'comparer_dates');   // On trie les cours en fonction de leur date
 
 function duree($h1, $m1, $h2, $m2) {
     $heure1 = $h1 + ($m1)/60;
     $heure2 = $h2 + ($m2)/60;
-    return $heure1 - $heure2;
-    }
-
+    return abs($heure1 - $heure2);
+}
 
 $ljours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
 
@@ -110,40 +114,41 @@ foreach ($events as $e) {
     }
 }
 
-echo '<div id="tableau">';
+echo '<div id="tableau">';?>
 
-foreach($ljours as $jour) {
-    echo '<div class="jour">';
-    echo '<div class="nomJour"><h1>'.$jour.'</h1></div>';
-    echo '<div class="dateJour">'.date("d", strtotime("last $days[$jour] +$nSemaine weeks", strtotime($date_ref))).'</div>';
-    echo '<div class="eventsJour">';
+<div id="heures">
+    <?php for ($h = 7; $h<=21; $h++):?>
+    <p style="top: <?php echo ($h-5)*50 - 25?>px"><?php echo $h ?>:00</p>
+    <?php endfor ?>
+</div>
 
-    $heureDernier = 07;
-    $minDernier = 0;
+<?php foreach($ljours as $jour) : ?>
+<div class="jour">
+    <div class="nomJour">
+        <h1><?php echo $jour ?></h1>
+        <div class="date"><?php echo date("d", strtotime("last $days[$jour] +$nSemaine weeks", strtotime($date_ref))) ?></div>
+    </div>
+    <div class="eventsJour">
 
-    foreach($eventsJour[$jour] as $e) {
-        $taille = duree($e['heure2'], $e['minute2'], $e['heure1'], $e['minute1'])*50;
-        $tempsAvant = duree($e['heure1'], $e['minute1'], $heureDernier, $minDernier)*50;
+    <?php $heureDernier = 07;
+    $minDernier = 0; ?>
+    <?php foreach($eventsJour[$jour] as $e) : ?>
+        <?php $hauteur = duree($e['heure2'], $e['minute2'], $e['heure1'], $e['minute1'])*50;
+        $tempsAvant = duree($e['heure1'], $e['minute1'], $heureDernier, $minDernier)*50;?>
 
-        echo '<div class="event" style="height: '.$taille.'px; margin-top: '.$tempsAvant.'px">';
+        <div class="event" style=" <?php echo 'height:  '.$hauteur.'px; margin-top: '.$tempsAvant.'px' ?>">
            
-        echo '<h2 class="event__title">' . $e['titre'] . '</h2>';
-        
-        /*echo '<div class="event__location">' . $e['location'] . '</div>';*/
+            <h2 class="event__title"><?php echo $e['titre'] ?></h2>
+            <div class="event__location"><?php echo $e['location'] ?></div>
+            <div class="event__date"><p><?php echo $e['heure1'].':'.$e['minute1'].' à '.$e['heure2'].':'.$e['minute2'] ?></p></div>
 
-        /*echo '<div class="event__description">' . str_replace('\n', '<br>', $e['description']) . '</div>';*/
+        </div>
+        <?php $heureDernier = $e['heure2'];
+        $minDernier = $e['minute2']; ?>
+    <?php endforeach?>
+</div>
+</div>
 
-        /*echo '<div class="event__date">'.'<p>'.$e['jour'].'/'.$e['mois'].'/'.$e['annee'].'</p>'.'<p>'.$e['heure1'].':'.$e['minute1'].' à '.$e['heure2'].':'.$e['minute2'].'</p></div>';*/
+<?php endforeach ?>
 
-        echo '<p>'.$e['date']->format('H:i').'</p>';
-
-        echo '</div>';
-
-        $heureDernier = $e['heure2'];
-        $minDernier = $e['minute2'];
-    }
-    echo '</div></div>';
-}
-
-echo '</div>';
-?>
+</div>
